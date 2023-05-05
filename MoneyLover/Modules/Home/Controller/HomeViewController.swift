55a61c9005740d5152bdd737bca6507daa2b1959
xhiew <nxhieu21@gmail.com>
2023-05-05 15:47:48 +0700

    fix bug bar chart

--- MoneyLover/Modules/Home/Controller/HomeViewController.swift
+++ MoneyLover/Modules/Home/Controller/HomeViewController.swift
@@ -9,18 +9,18 @@ import UIKit
 
 class HomeViewController: BaseViewController {
 
-  @IBOutlet weak var homeTableView: UITableView!
+	@IBOutlet weak var homeTableView: UITableView!
 	
 	var homeManager = HomeManager()
 
-  override func viewDidLoad() {
-    super.viewDidLoad()
-    configTableView()
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		configTableView()
 		homeManager.attachView(delegate: self)
 		NotificationCenter.default.addObserver(self, selector: #selector(updateUI), name: .changedAmount, object: nil)
 		NotificationCenter.default.addObserver(self, selector: #selector(refetchTransactionInRealm(_:)), name: .createdNewTransaction, object: nil)
 		NotificationCenter.default.addObserver(self, selector: #selector(reloadView), name: .deletedTransaction, object: nil)
-  }
+	}
 
 	@objc private func updateUI() {
 		homeManager.reloadRecentViewAndWallet()
@@ -36,41 +36,41 @@ class HomeViewController: BaseViewController {
 		}
 	}
 
-  func configTableView() {
-    homeTableView.dataSource = self
-    homeTableView.separatorStyle = .none
-    homeTableView.register(AccountBalanceCell.nib(), forCellReuseIdentifier: AccountBalanceCell.identifier)
-    homeTableView.register(MyWalletCell.nib(), forCellReuseIdentifier: MyWalletCell.identifier)
-    homeTableView.register(HeaderCell.nib(), forCellReuseIdentifier: HeaderCell.identifier)
-    homeTableView.register(PromotionCell.nib(), forCellReuseIdentifier: PromotionCell.identifier)
-    homeTableView.register(PersonalPlanCell.nib(), forCellReuseIdentifier: PersonalPlanCell.identifier)
-    homeTableView.register(ExpenseReportCell.nib(), forCellReuseIdentifier: ExpenseReportCell.identifier)
-    homeTableView.register(RecentTransactionCell.nib(), forCellReuseIdentifier: RecentTransactionCell.identifier)
+	func configTableView() {
+		homeTableView.dataSource = self
+		homeTableView.separatorStyle = .none
+		homeTableView.register(AccountBalanceCell.nib(), forCellReuseIdentifier: AccountBalanceCell.identifier)
+		homeTableView.register(MyWalletCell.nib(), forCellReuseIdentifier: MyWalletCell.identifier)
+		homeTableView.register(HeaderCell.nib(), forCellReuseIdentifier: HeaderCell.identifier)
+		homeTableView.register(PromotionCell.nib(), forCellReuseIdentifier: PromotionCell.identifier)
+		homeTableView.register(PersonalPlanCell.nib(), forCellReuseIdentifier: PersonalPlanCell.identifier)
+		homeTableView.register(ExpenseReportCell.nib(), forCellReuseIdentifier: ExpenseReportCell.identifier)
+		homeTableView.register(RecentTransactionCell.nib(), forCellReuseIdentifier: RecentTransactionCell.identifier)
 		homeTableView.register(TheEndCell.nib(), forCellReuseIdentifier: TheEndCell.identifier)
-  }
+	}
 
 }
 
 //MARK: - Conform UITableViewDataSource
 extension HomeViewController: UITableViewDataSource {
-  func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-    return Commons.shared.numberOfRowsInSection
-  }
+	func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+		return Commons.shared.numberOfRowsInSection
+	}
 
-  func numberOfSections(in tableView: UITableView) -> Int {
-    return homeManager.homeItem.count
-  }
+	func numberOfSections(in tableView: UITableView) -> Int {
+		return homeManager.homeItem.count
+	}
 
-  func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-    switch homeManager.homeItem[indexPath.section] {
-    case .accBalance:
-      let cell = homeTableView.dequeueReusableCell(withIdentifier: AccountBalanceCell.identifier, for: indexPath) as? AccountBalanceCell
-      guard let cell = cell else { return UITableViewCell() }
+	func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+		switch homeManager.homeItem[indexPath.section] {
+		case .accBalance:
+			let cell = homeTableView.dequeueReusableCell(withIdentifier: AccountBalanceCell.identifier, for: indexPath) as? AccountBalanceCell
+			guard let cell = cell else { return UITableViewCell() }
 			cell.accountBalance = self.homeManager.currentAmount
-      return cell
-    case .myWallet:
-      let cell = homeTableView.dequeueReusableCell(withIdentifier: MyWalletCell.identifier, for: indexPath) as? MyWalletCell
-      guard let cell = cell else { return UITableViewCell() }
+			return cell
+		case .myWallet:
+			let cell = homeTableView.dequeueReusableCell(withIdentifier: MyWalletCell.identifier, for: indexPath) as? MyWalletCell
+			guard let cell = cell else { return UITableViewCell() }
 			cell.currentAmount = homeManager.currentAmount
 			cell.changeAmount = { [weak self] in
 				let alert = UIAlertController(title: Resource.ActionTitle.fineTuneWallet, message: nil, preferredStyle: .actionSheet)
@@ -82,27 +82,27 @@ extension HomeViewController: UITableViewDataSource {
 				alert.addAction(UIAlertAction(title: Resource.ActionTitle.cancel, style: .cancel, handler: nil))
 				self?.present(alert, animated: true, completion: nil)
 			}
-      return cell
-    case .header(title: let title):
-      let cell = homeTableView.dequeueReusableCell(withIdentifier: HeaderCell.identifier, for: indexPath) as? HeaderCell
-      guard let cell = cell else { return UITableViewCell() }
-      cell.setTitle(title: title)
-      return cell
-    case .promo:
-      let cell = homeTableView.dequeueReusableCell(withIdentifier: PromotionCell.identifier, for: indexPath) as? PromotionCell
-      guard let cell = cell else { return UITableViewCell() }
+			return cell
+		case .header(title: let title):
+			let cell = homeTableView.dequeueReusableCell(withIdentifier: HeaderCell.identifier, for: indexPath) as? HeaderCell
+			guard let cell = cell else { return UITableViewCell() }
+			cell.setTitle(title: title)
+			return cell
+		case .promo:
+			let cell = homeTableView.dequeueReusableCell(withIdentifier: PromotionCell.identifier, for: indexPath) as? PromotionCell
+			guard let cell = cell else { return UITableViewCell() }
 
-      return cell
-    case .personalPlan:
-      let cell = homeTableView.dequeueReusableCell(withIdentifier: PersonalPlanCell.identifier, for: indexPath) as? PersonalPlanCell
-      guard let cell = cell else { return UITableViewCell() }
+			return cell
+		case .personalPlan:
+			let cell = homeTableView.dequeueReusableCell(withIdentifier: PersonalPlanCell.identifier, for: indexPath) as? PersonalPlanCell
+			guard let cell = cell else { return UITableViewCell() }
 			cell.registedNotification = {
 				Commons.shared.showToast(image: Resource.Image.systemSuccess?.withTintColor(Theme.shared.greenButtonColor, renderingMode: .alwaysOriginal), title: Resource.NotiTitle.successTitleVi, subtitle: Resource.NotiTitle.subscribedNotiSubtitle)
 			}
-      return cell
-    case .expenseReport:
-      let cell = homeTableView.dequeueReusableCell(withIdentifier: ExpenseReportCell.identifier, for: indexPath) as? ExpenseReportCell
-      guard let cell = cell else { return UITableViewCell() }
+			return cell
+		case .expenseReport:
+			let cell = homeTableView.dequeueReusableCell(withIdentifier: ExpenseReportCell.identifier, for: indexPath) as? ExpenseReportCell
+			guard let cell = cell else { return UITableViewCell() }
 
 			cell.maxWeeklyExpenses = homeManager.maxWeeklyExpenses
 			cell.maxMonthlyExpenses = homeManager.maxMonthlyExpenses
@@ -110,11 +110,8 @@ extension HomeViewController: UITableViewDataSource {
 			cell.totalThisWeekExpenses = homeManager.totalThisWeekExpenses
 			cell.totalLastMonthExpenses = homeManager.totalLastMonthExpenses
 			cell.totalLastWeekExpenses = homeManager.totalLastWeekExpenses
-
-			cell.showMaxExpenses(maxExpenses: homeManager.maxMonthlyExpenses, totalExpense: homeManager.totalThisMonthExpenses)
-			cell.showtotalExpenses(amount: homeManager.totalThisMonthExpenses)
-			cell.performChart(firstYValue: homeManager.totalLastMonthExpenses, secondYValue: homeManager.totalThisMonthExpenses)
-
+			cell.reloadChart()
+			
 			cell.showPieChart = { [weak self] segCtrlType in
 				guard let self = self else { return }
 				switch segCtrlType {
@@ -130,21 +127,21 @@ extension HomeViewController: UITableViewDataSource {
 					self.present(vc, animated: true, completion: nil)
 				}
 			}
-      return cell
-    case .recentTransaction:
-      let cell = homeTableView.dequeueReusableCell(withIdentifier: RecentTransactionCell.identifier, for: indexPath) as? RecentTransactionCell
-      guard let cell = cell else { return UITableViewCell() }
+			return cell
+		case .recentTransaction:
+			let cell = homeTableView.dequeueReusableCell(withIdentifier: RecentTransactionCell.identifier, for: indexPath) as? RecentTransactionCell
+			guard let cell = cell else { return UITableViewCell() }
 			cell.showItems(transactions: homeManager.recentTransactions)
 			cell.tapOnCell = { [weak self] in
 				self?.tabBarController?.selectedIndex = 2
 			}
-      return cell
+			return cell
 		case .theEnd:
 			let cell = homeTableView.dequeueReusableCell(withIdentifier: TheEndCell.identifier, for: indexPath) as? TheEndCell
 			guard let cell = cell else { return UITableViewCell() }
 			return cell
-    }
-  }
+		}
+	}
 }
 
 //MARK: - Conform HomeManagerDelegate
--- MoneyLover/Modules/Home/View/Cell/ExpenseReportCell.swift
+++ MoneyLover/Modules/Home/View/Cell/ExpenseReportCell.swift
@@ -15,17 +15,16 @@ enum SegCtrlType {
 
 class ExpenseReportCell: BaseTableViewCell {
 
-  @IBOutlet weak var segmentedControl: UISegmentedControl!
-  @IBOutlet weak var totalExpenses: UILabel!
-  @IBOutlet weak var barChart: BarChartView!
-  @IBOutlet weak var stackExpenses: UIStackView!
-  @IBOutlet weak var heightForStackView: NSLayoutConstraint!
-  @IBOutlet weak var stateLabel: UILabel!
+	@IBOutlet weak var segmentedControl: UISegmentedControl!
+	@IBOutlet weak var totalExpenses: UILabel!
+	@IBOutlet weak var barChart: BarChartView!
+	@IBOutlet weak var stackExpenses: UIStackView!
+	@IBOutlet weak var heightForStackView: NSLayoutConstraint!
+	@IBOutlet weak var stateLabel: UILabel!
 	@IBOutlet weak var subTitle: UILabel!
 
 	var showPieChart: ((SegCtrlType) -> Void)?
 	let numberFormatter = NumberFormatter()
-
 	var maxWeeklyExpenses: [Transaction]?
 	var maxMonthlyExpenses: [Transaction]?
 	var totalThisWeekExpenses = 0.0
@@ -34,19 +33,38 @@ class ExpenseReportCell: BaseTableViewCell {
 	var totalLastMonthExpenses = 0.0
 	let segCtrlTypes: [SegCtrlType] = [.week, .month]
 
-  override func awakeFromNib() {
-    super.awakeFromNib()
-  }
+	override func awakeFromNib() {
+		super.awakeFromNib()
+	}
 
-  override func configUI() {
-    super.configUI()
-    configSegmentedControl()
+	override func configUI() {
+		super.configUI()
+		configSegmentedControl()
 		configChart()
-  }
+	}
 
-  override func setSelected(_ selected: Bool, animated: Bool) {
-    super.setSelected(selected, animated: animated)
-  }
+	override func setSelected(_ selected: Bool, animated: Bool) {
+		super.setSelected(selected, animated: animated)
+	}
+
+	func reloadChart() {
+		switch segCtrlTypes[segmentedControl.selectedSegmentIndex] {
+		case .week:
+			showMaxExpenses(maxExpenses: maxWeeklyExpenses, totalExpense: totalThisWeekExpenses)
+			showtotalExpenses(amount: totalThisWeekExpenses)
+			performChart(firstYValue: totalLastWeekExpenses, secondYValue: totalThisWeekExpenses)
+
+			subTitle.text = Resource.Title.totalThisWeekExpenses
+			barChart.xAxis.valueFormatter = IndexAxisValueFormatter(values: [Resource.DateTitle.lastWeek, Resource.DateTitle.thisWeek])
+		case .month:
+			showMaxExpenses(maxExpenses: maxMonthlyExpenses, totalExpense: totalThisMonthExpenses)
+			showtotalExpenses(amount: totalThisMonthExpenses)
+			performChart(firstYValue: totalLastMonthExpenses, secondYValue: totalThisMonthExpenses)
+
+			subTitle.text = Resource.Title.totalThisMonthExpenses
+			barChart.xAxis.valueFormatter = IndexAxisValueFormatter(values: [Resource.DateTitle.lastMonth, Resource.DateTitle.thisMonth])
+		}
+	}
 
 	private func configNumberFomatter(currency: Double) {
 		numberFormatter.numberStyle = .currency
@@ -66,26 +84,26 @@ class ExpenseReportCell: BaseTableViewCell {
 		}
 	}
 
-  private func configChart() {
-    barChart.isUserInteractionEnabled = false
-    barChart.backgroundColor = .clear
-    barChart.leftAxis.enabled = false
-    barChart.leftAxis.spaceBottom = 0.0
-    barChart.rightAxis.drawGridLinesEnabled = false
-    barChart.rightAxis.drawAxisLineEnabled = false
-    barChart.rightAxis.labelCount = 0
-    barChart.xAxis.labelCount = 0
-    barChart.xAxis.labelPosition = .bottom
+	private func configChart() {
+		barChart.isUserInteractionEnabled = false
+		barChart.backgroundColor = .clear
+		barChart.leftAxis.enabled = false
+		barChart.leftAxis.spaceBottom = 0.0
+		barChart.rightAxis.drawGridLinesEnabled = false
+		barChart.rightAxis.drawAxisLineEnabled = false
+		barChart.rightAxis.labelCount = 0
+		barChart.xAxis.labelCount = 0
+		barChart.xAxis.labelPosition = .bottom
 		barChart.rightAxis.valueFormatter = DefaultAxisValueFormatter(formatter: numberFormatter)
-    barChart.xAxis.valueFormatter = IndexAxisValueFormatter(values: [Resource.DateTitle.lastMonth, Resource.DateTitle.thisMonth])
-    barChart.xAxis.drawGridLinesEnabled = false
-    barChart.legend.enabled = false
+		barChart.xAxis.valueFormatter = IndexAxisValueFormatter(values: [Resource.DateTitle.lastMonth, Resource.DateTitle.thisMonth])
+		barChart.xAxis.drawGridLinesEnabled = false
+		barChart.legend.enabled = false
 
-  }
+	}
 
-  private func configSegmentedControl() {
+	private func configSegmentedControl() {
 		segmentedControl.setTitleTextAttributes([NSAttributedString.Key.font: UIFont.systemFont(ofSize: 16)], for: .normal)
-  }
+	}
 
 	func showtotalExpenses(amount: Double) {
 		totalExpenses.text = amount.formatMoneyNumber() + " " + Resource.Title.vnd
@@ -94,16 +112,16 @@ class ExpenseReportCell: BaseTableViewCell {
 	func performChart(firstYValue: Double = 0.0, secondYValue: Double = 0.0) {
 		let currency = firstYValue > secondYValue ? firstYValue : secondYValue
 		configNumberFomatter(currency: currency)
-    let lastMonth = BarChartDataEntry(x: 0.0, yValues: [firstYValue])
-    let thisMonth = BarChartDataEntry(x: 1.0, yValues: [secondYValue])
-    let set = BarChartDataSet(entries: [lastMonth, thisMonth])
-    set.colors = ChartColorTemplates.material()
-    set.drawValuesEnabled = false
-    let data = BarChartData(dataSet: set)
-    data.barWidth = 0.5
-    barChart.data = data
-    barChart.animate(yAxisDuration: 0.5)
-  }
+		let lastMonth = BarChartDataEntry(x: 0.0, yValues: [firstYValue])
+		let thisMonth = BarChartDataEntry(x: 1.0, yValues: [secondYValue])
+		let set = BarChartDataSet(entries: [lastMonth, thisMonth])
+		set.colors = ChartColorTemplates.material()
+		set.drawValuesEnabled = false
+		let data = BarChartData(dataSet: set)
+		data.barWidth = 0.5
+		barChart.data = data
+		barChart.animate(yAxisDuration: 0.5)
+	}
 
 	func showMaxExpenses(maxExpenses: [Transaction]?, totalExpense: Double) {
 		clearStack()
@@ -114,18 +132,18 @@ class ExpenseReportCell: BaseTableViewCell {
 		} else {
 			stateLabel.isHidden = true
 		}
-    for maxExpense in maxExpenses {
-      let view = TransactionItemView()
+		for maxExpense in maxExpenses {
+			let view = TransactionItemView()
 			let percent = ((maxExpense.amount ?? 0.0) / totalExpense).toPercent()
 			view.setupReportTransactionUI(image: maxExpense.group?.image, name: maxExpense.group?.name, amount: maxExpense.amount, percent: percent)
-      stackExpenses.addArrangedSubview(view)
-      view.snp.makeConstraints { make in
-        make.width.equalTo(stackExpenses)
-      }
-    }
+			stackExpenses.addArrangedSubview(view)
+			view.snp.makeConstraints { make in
+				make.width.equalTo(stackExpenses)
+			}
+		}
 		let numberOfItem = maxExpenses.count
-    heightForStackView.constant = Demension.shared.heightForItemTransaction * CGFloat(numberOfItem) + Demension.shared.stackSpacing * CGFloat(numberOfItem - 1)
-  }
+		heightForStackView.constant = Demension.shared.heightForItemTransaction * CGFloat(numberOfItem) + Demension.shared.stackSpacing * CGFloat(numberOfItem - 1)
+	}
 
 	private func clearStack() {
 		stackExpenses.arrangedSubviews.forEach({$0.removeFromSuperview()})
@@ -138,22 +156,7 @@ class ExpenseReportCell: BaseTableViewCell {
 	}
 
 	@IBAction func segCtrlValueChanged(_ sender: UISegmentedControl) {
-		switch segCtrlTypes[sender.selectedSegmentIndex] {
-		case .week:
-			showMaxExpenses(maxExpenses: maxWeeklyExpenses, totalExpense: totalThisWeekExpenses)
-			showtotalExpenses(amount: totalThisWeekExpenses)
-			performChart(firstYValue: totalLastWeekExpenses, secondYValue: totalThisWeekExpenses)
-
-			subTitle.text = Resource.Title.totalThisWeekExpenses
-			barChart.xAxis.valueFormatter = IndexAxisValueFormatter(values: [Resource.DateTitle.lastWeek, Resource.DateTitle.thisWeek])
-		case .month:
-			showMaxExpenses(maxExpenses: maxMonthlyExpenses, totalExpense: totalThisMonthExpenses)
-			showtotalExpenses(amount: totalThisMonthExpenses)
-			performChart(firstYValue: totalLastMonthExpenses, secondYValue: totalThisMonthExpenses)
-
-			subTitle.text = Resource.Title.totalThisMonthExpenses
-			barChart.xAxis.valueFormatter = IndexAxisValueFormatter(values: [Resource.DateTitle.lastMonth, Resource.DateTitle.thisMonth])
-		}
+		reloadChart()
 	}
 	
 }
